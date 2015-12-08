6ed5d4fd0d0a2850510ed37b8bf8a307058791f2
john <john-cahill1997@hotmail.com>
2015-12-08 13:53:43 +0000

    php

--- changexml.php
+++ changexml.php
@@ -9,9 +9,9 @@ $title = $_POST["title"];
 $author = $_POST["author"];
 echo $title;
 
-if (file_exists('product.xml')) {
+if (file_exists('bookList.xml')) {
     //loads the xml and returns a simplexml object
-    $xml = simplexml_load_file('product.xml');
+    $xml = simplexml_load_file('bookList.xml');
     //print_r($xml);
     
     //transforming the object in xml format
@@ -43,87 +43,31 @@ file_put_contents('product.xml', $xml->saveXML());
 
 
 } else {
-    exit('Failed to open product.xml.'); 
+    exit('Failed to open bookList.xml.'); 
 }
-
-
-
-?>
-
-//rss feed
-
-<?php
-/*
- * Loading a XML from a file, adding new elements and editing elements
- */
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-//get entry from form
-$type = $_POST["type"];
-$name = $_POST["name"];
-$ram = $_POST["ram"];
-$price = $_POST["price"];
-echo $type;
-
-if (file_exists('product.xml')) {
-    //loads the xml and returns a simplexml object
-    $xml = simplexml_load_file('product.xml');
-    //print_r($xml);
-    
-    //transforming the object in xml format
-    $xmlFormat = $xml->asXML();
-    //echo htmlentities($xmlFormat);
-
-
-    //displaying the element in proper format
-  echo '<u><b>This is the xml code from test2.xml:</b></u>
-     <br /><br />
-     <pre>' . htmlentities($xmlFormat, ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre><br /><br />'; 
-
-    //adding new child to the xml
-    $newChild = $xml->addChild('secondhand');
-    $newChild->addChild('type', $type);
-    $newChild->addChild('name', $name);
-    $newChild->addChild('ram', $ram);
-    $newChild->addChild('price', $price);
-  
-    //transforming the object in xml format
-    $xmlFormat = $xml->asXML();
-   // echo htmlentities($xmlFormat);
-
-    //displaying the element in proper format
-   echo '<u><b>This is the xml code from product.xml with new elements added:</b></u>
-     <br /><br />
-     <pre>' . htmlentities($xmlFormat, ENT_COMPAT | ENT_HTML401, "ISO-8859-1") . '</pre>';
-
-file_put_contents('product.xml', $xml->saveXML());
-
-
-} else {
-    exit('Failed to open product.xml.'); 
-}
-    
-?>
-11:39
-Can I you send us a link the rest of that xsl rss thingy? tongue emoticon
-
-writeRSS();
+/*writeRSS();
 function writeRSS(){
     if (file_exists('rss.xml')) {
-        $type = $_POST["type"];
-        $name = $_POST["name"];
-        $ram = $_POST["ram"];
-        $price = $_POST["price"];
+        $title = $_POST["title"];
+        $author = $_POST["author"];
+        echo $title;
+
         
-        $title = $name;
-        $description = $type .", ".$ram.", ".$price;
+        $title = $title;
+        $description = $author;
         
         //loads the xml and returns a simplexml object
         $rssxml = simplexml_load_file('rss.xml');
         $newChild = $rssxml->channel->addChild('item');
         $newChild->addChild('title', $title);
-        $newChild->addChild('link', 'product.xml');
+        $newChild->addChild('link', 'bookList.xml');
         $newChild->addChild('description', $description);
         file_put_contents('rss.xml', $rssxml->asXML());
-    }
-}
+    }*/
+
+
+?>
+
+
+
+
